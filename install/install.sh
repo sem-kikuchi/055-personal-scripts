8caa2494b652a52b0f517d64441de2301677ef1e
Shintaro Kikuchi <kikuchi@studiosem.co.jp>
2022-11-18 19:19:40 +0900

    no message

--- install/install.sh
+++ install/install.sh
@@ -9,3 +9,15 @@ sudo cp amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon
 
 # Start the agent
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
+
+
+# Python (statsd) Install
+sudo amazon-linux-extras install python3.8 -y
+sudo pip3.8 install statsd watchdog pandas
+
+# custom metrics collect in background
+sudo chmod +x statsd_monitor.py 
+
+pyhon3.8 statsd_monitor.py metrics &
+pyhon3.8 statsd_monitor.py metrics_1 &
+
