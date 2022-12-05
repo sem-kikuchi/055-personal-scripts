248ec041d2a3b4acc805cbeb277f08834594f752
Shintaro Kikuchi <kikuchi@studiosem.co.jp>
2022-12-05 19:06:40 +0900

    no message

--- cp_from_win.sh
+++ cp_from_win.sh
@@ -6,7 +6,8 @@ echo $CURRENT
 pushd $CURRENT
 
 rm -rf ../linux_build/*
-scp -r kikuchi@192.168.45.27:D:\\prj055-2\\trunk\\Development\\Games\\PRJ055\\Saved\\StagedBuilds\\LinuxServer ../linux_build
+
+scp -r kikuchi@192.168.45.27:D:/prj055-2/trunk/Development/Games/PRJ055/Saved/StagedBuilds/LinuxServer ../linux_build
 
 popd
 
